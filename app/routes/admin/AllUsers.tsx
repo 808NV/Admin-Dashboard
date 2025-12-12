7bed109fb93ec909e5735b463656f5db12c163e1
808nv <oali229@gmail.com>
2025-12-12 05:02:26 +0300

    add dynamic header components
    users can now read the title and description of
    each page

--- app/routes/admin/AllUsers.tsx
+++ app/routes/admin/AllUsers.tsx
@@ -1,6 +1,10 @@
+import { Header } from "components"
+
 const AllUsers = () => {
   return (
-    <div>AllUsers</div>
+    <main className="dashboard wrapper w-full max-w-7xl mx-auto px-4 lg:px-8">
+      <Header title="Trips page" description="Track current user in real time" />
+    </main>
   )
 }
 
